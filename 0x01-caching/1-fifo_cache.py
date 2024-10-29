0716497c427284fb2fa5d97ddfad9489e0774e4e
AMINEXD33 <amine.gamer222@gmail.com>
2024-10-29 23:52:28 +0100

    task 1

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
-""" FIFO CACHING MODULE
+
+"""Task 1: FIFO caching
 """
-from base_caching import BaseCaching
+
+
 from collections import OrderedDict
+from base_caching import BaseCaching
 
 
 class FIFOCache(BaseCaching):
-    """FIFOCache defines:
-    - constants of your caching system
-    - where your data are stored (in a dictionary)
+    """A class `FIFOCache` that inherits from
+    `BaseCaching` and is a caching system.
     """
 
     def __init__(self):
@@ -16,25 +18,60 @@ class FIFOCache(BaseCaching):
         self.cache_data = OrderedDict()
 
     def put(self, key, item):
+        """assign to the dictionary `self.cache_data` the
+        `item` value for the key `key`
         """
-        a function that puts a new item into cache
-        with respect to the limit specified in MAX_ITEMS
-        """
+
         if key is None or item is None:
             return
-
-        self.cache_data[key] = item
 
         if len(self.cache_data) > BaseCaching.MAX_ITEMS:
             first_key, _ = self.cache_data.popitem(last=False)
             print(f"DISCARD: {first_key}")
 
+        self.cache_data[key] = item
+
     def get(self, key):
-        """
-        a function that gets a value from cache based on a key
-        it return None if the key is None or doesn't exist
-        """
-        if key in None:
-            return None
-        data = self.cache_data.get(key)
-        return data
+        """return the value in `self.cache_data` linked to `key`"""
+        return self.cache_data.get(key, None)
+
+
+# #!/usr/bin/env python3
+# """ FIFO CACHING MODULE
+# """
+# from base_caching import BaseCaching
+# from collections import OrderedDict
+
+
+# class FIFOCache(BaseCaching):
+#     """FIFOCache defines:
+#     - constants of your caching system
+#     - where your data are stored (in a dictionary)
+#     """
+
+#     def __init__(self):
+#         super().__init__()
+#         self.cache_data = OrderedDict()
+
+#     def put(self, key, item):
+#         """
+#         a function that puts a new item into cache
+#         with respect to the limit specified in MAX_ITEMS
+#         """
+#         if key is None or item is None:
+#             return
+
+#         self.cache_data[key] = item
+
+#         if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+#             first_key, _ = self.cache_data.popitem(last=False)
+#             print(f"DISCARD: {first_key}")
+
+#     def get(self, key):
+#         """
+#         a function that gets a value from cache based on a key
+#         it return None if the key is None or doesn't exist
+#         """
+#         if key in None:
+#             return None
+#         return self.cache_data.get(key)
