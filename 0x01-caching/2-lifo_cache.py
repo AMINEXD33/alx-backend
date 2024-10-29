9c622153ba36cd610facbb5682af85cf61c767d8
AMINEXD33 <amine.gamer222@gmail.com>
2024-10-30 00:03:57 +0100

    task 2

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -22,13 +22,16 @@ class LIFOCache(BaseCaching):
         """
         if key is None or item is None:
             return
+        if key not in self.cache_data:
+            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+                last_key, _ = self.cache_data.popitem(True)
+                print("DISCARD:", last_key)
+        self.cache_data[key] = item
+        self.cache_data.move_to_end(key, last=True)
+
 
         self.cache_data[key] = item
-
-        if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
-            first_key, _ = self.cache_data.popitem(last=True)
-            print(f"DISCARD: {first_key}")
-
+        self.cache_data.move_to_end(key, last=True)
     def get(self, key):
         """
         a function that gets a value from cache based on a key
